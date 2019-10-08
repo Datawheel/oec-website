caa6e93c3a0c6e7b70b4bca48e27e49715fa56e7
James Ferrell <hello@jamesferrell.me>
2019-10-08 12:30:15 -0400

    adds NavGroup classes and open/close logic

--- app/components/NavGroup.jsx
+++ app/components/NavGroup.jsx
@@ -1,6 +1,5 @@
-import React, {Component} from "react";
-import {Link} from "react-router";
-
+import React, {Component, Fragment} from "react";
+import {Icon} from "@blueprintjs/core";
 import "./NavGroup.css";
 
 export default class NavGroup extends Component {
@@ -11,47 +10,66 @@ export default class NavGroup extends Component {
     };
   }
 
+  /** when tabbing out of the nav group, collapse it */
+  onBlur(e) {
+    const currentTarget = e.currentTarget;
+
+    setTimeout(() => {
+      if (!currentTarget.contains(document.activeElement)) {
+        this.setState({isOpen: false});
+      }
+    }, 0);
+  }
+
+  /** the link markup is the same whether it's rendered in a nested list or not */
+  renderLink(item) {
+    return <a href={item.url} className="nav-group-link" onFocus={() => this.setState({isOpen: true})}>
+      {item.icon &&
+        <img className="nav-group-link-icon" src={item.icon} alt="" />
+      }
+      {item.title}
+    </a>;
+  }
+
   render() {
     const {title, items} = this.props;
     const {isOpen} = this.state;
 
-    return <li>
-      <button>
-        <span className="u-visually-hidden">{isOpen ? "hide " : "show "}</span> {title}
-      </button>
-      {items && items.length &&
-        <ul>
-          {items.map(item =>
-            <li key={item.title}>
-              {item.items && item.items.length
+    return (
+      <li className={`nav-group ${isOpen ? "is-open" : "is-closed"}`} onBlur={e => this.onBlur(e)}>
+        {/* click the title to toggle the menu */}
+        <button className="nav-group-button" onClick={() => this.setState({isOpen: !isOpen})}>
+          <span className="u-visually-hidden">{isOpen ? "hide" : "show"} </span>
+          <span className="nav-group-button-text">{title} </span>
+          <Icon icon="caret-down" className="nav-group-button-icon" />
+        </button>
 
-                // nested list
-                ? <React.Fragment>
-                  <p>{item.title}</p>
-                  <ul>{item.items.map(nestedItem =>
-                    <li key={nestedItem.title}>
-                      <a href={nestedItem.url}>
-                        {nestedItem.icon &&
-                          <img src={nestedItem.icon} alt="" />
-                        }
-                        {nestedItem.title}
-                      </a>
-                    </li>
-                  )}</ul>
-                </React.Fragment>
+        {/* loop through nav links */}
+        {items && items.length &&
+          <ul className="nav-group-list">
+            {items.map(item =>
+              <li className="nav-group-item" key={item.title}>
+                {item.items && item.items.length
+                  // nested items array; render them in a nested list
+                  ? <Fragment>
+                    <p className="nav-group-subtitle heading">{item.title}</p>
+                    <ul className="nav-group-list nav-group-nested-list">
+                      {item.items.map(nestedItem =>
+                        <li className="nav-group-item nav-group-nested-item" key={nestedItem.title}>
+                          {this.renderLink(nestedItem)}
+                        </li>
+                      )}
+                    </ul>
+                  </Fragment>
 
-                // no nested list
-                : <a href={item.url}>
-                  {item.icon &&
-                    <img src={item.icon} alt="" />
-                  }
-                  {item.title}
-                </a>
-              }
-            </li>
-          )}
-        </ul>
-      }
-    </li>;
+                  // no nested items array; just render the link
+                  : this.renderLink(item)
+                }
+              </li>
+            )}
+          </ul>
+        }
+      </li>
+    );
   }
 }
