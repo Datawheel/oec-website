4a987ed1b841218b94a99453a074d3b2ca6a8151
James Ferrell <hello@jamesferrell.me>
2019-10-15 15:08:11 -0400

    fixes footer markup

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -26,11 +26,11 @@ class Footer extends React.Component {
         </ul>
         <p className="footer-legal u-font-xxs u-margin-bottom-off">
           The Observatory of Economic Complexity by <a href="http://alexandersimoes.com/">Alexander Simoes</a> is licensed under a <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>. Permissions beyond the scope of this license may be available <a href="/permissions/">here</a>.
-          <div className="footer-license-container u-margin-top-xs">
-            <a className="license" rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">
-              <img alt="Creative Commons License" src="//i.creativecommons.org/l/by-sa/3.0/80x15.png" />
-            </a>
-          </div>
+        </p>
+        <p className="footer-license-container u-margin-top-xs">
+          <a className="license" rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">
+            <img alt="Creative Commons License" src="//i.creativecommons.org/l/by-sa/3.0/80x15.png" />
+          </a>
         </p>
       </div>
     </div>;
