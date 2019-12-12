188df481f0f9a7b215d6b5b6862a07bffa706acd
cnavarreteliz <cnavarreteliz@gmail.com>
2019-12-12 13:47:13 -0300

    includes network for tech space

---
+++ app/pages/Vizbuilder/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {connect} from "react-redux";
+import {withNamespaces} from "react-i18next";
+
+class Vizbuilder extends React.Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+export default withNamespaces()(connect())(Vizbuilder);
