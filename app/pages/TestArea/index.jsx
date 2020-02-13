564b0eef92fc788b112aacf6c02052dd15782971
Francisco Abarzúa <git.id@frabarz.cl>
2020-02-13 18:53:27 -0300

    updates TestArea

--- app/pages/TestArea/index.jsx
+++ app/pages/TestArea/index.jsx
@@ -1,19 +1,25 @@
 import axios from "axios";
 import React, {useEffect, useState} from "react";
-import SelectMultiSection from "./SelectMultiSection";
+import SelectMultiSection from "../../components/SelectMultiSection";
 
 const SECTION_DATA_URL =
   "https://api.oec.world/tesseract/data.jsonrecords?cube=trade_i_baci_a_92&drilldowns=HS4&measures=Trade+Value&parents=true&sparse=false";
 
 /** @type {React.FC<import("react-router").RouteComponentProps>} */
-const TestArea = props => {
-  console.log("render TestArea", props.routerParams);
-
-  const [sectionData, setSectionData] = useState([]);
+const TestArea = () => {
+  // selectedItems is an empty array at start
+  // it will be filled with objects with this shape:
+  // interface SelectedItem {
+  //   name: string;
+  //   id: number;
+  //   type: "Section" | "HS2" | "HS4";
+  //   color: string;
+  //   icon: string;
+  // }
   const [selectedItems, setSelectedItems] = useState([]);
 
-  const saveSelectedItem = item => setSelectedItems(selectedItems.concat(item));
-
+  // here I'm storing the values obtained from the ajax request
+  const [sectionData, setSectionData] = useState([]);
   useEffect(() => {
     axios.get(SECTION_DATA_URL).then(resp => {
       setSectionData(resp.data.data);
@@ -24,7 +30,21 @@ const TestArea = props => {
     <div className="test-area">
       <SelectMultiSection
         items={sectionData}
-        onItemSelect={saveSelectedItem}
+        onItemSelect={item => {
+          // item: SelectedItem
+          const nextItems = selectedItems.concat(item);
+          setSelectedItems(nextItems);
+        }}
+        onItemRemove={(evt, item) => {
+          // evt: MouseEvent<HTMLButtonElement>
+          // item: SelectedItem
+          evt.stopPropagation();
+          const nextItems = selectedItems.filter(i => i !== item);
+          setSelectedItems(nextItems);
+        }}
+        onClear={() => {
+          setSelectedItems([]);
+        }}
         selectedItems={selectedItems}
       />
     </div>
