d17d8dfb0d109320bc41b4c0324a9884d27e5603
jhmullen <jimmy.mullen@gmail.com>
2020-02-18 16:10:10 -0500

    adds front-end table stub

---
+++ app/pages/Matrix/Matrix.jsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+import React from "react";
+
+class Matrix extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      matrix: false
+    };
+  }
+
+  componentDidMount() {
+    axios.get("/api/matrix").then(resp => {
+      this.setState({matrix: resp.data});
+    });
+  }
+
+  render() {
+
+    const {matrix} = this.state;
+
+    if (!matrix) return null;
+
+    return (
+      <div className="matrix-page">
+        <h3>Data Availability Matrix</h3>
+        <table>
+          <tr>
+            <th>CubeName</th>
+            <th>Geo Resolution</th>
+            <th>Product Resolution</th>
+            <th>Time Resolution</th>
+            <th>Data Start</th>
+            <th>Data End</th>
+          </tr>
+          {matrix.map(row => 
+            <tr key={row.name}>
+              <td>{row.name}</td>
+              <td>-</td>
+              <td>-</td>
+              <td>{row.resolutions.time}</td>
+              <td>{row.start}</td>
+              <td>{row.end}</td>
+            </tr>
+          )}
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Matrix;
