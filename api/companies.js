da2a918e123778edf0226c08b3825a72de73f898
Alexander Simoes <alexandersimoes@gmail.com>
2020-02-27 17:16:35 -0500

    adds support for querying connect americas companies by hs code

--- api/companies.js
+++ api/companies.js
@@ -1,20 +1,58 @@
 const axios = require("axios");
 
-const caCountryIds = {afg: "1958", sxm: "2158", alb: "1960", dza: "1961", asm: "1962", and: "1963", ago: "1964", aia: "1965", ata: "1966", atg: "1967", arg: "1968", arm: "1969", abw: "1970", aus: "1971", aut: "1972", aze: "1973", bhs: "1974", bhr: "1975", bgd: "1976", brb: "1977", blr: "1978", bel: "1979", blz: "1980", ben: "1981", bmu: "1982", btn: "1983", bol: "1984", bes: "1985", bih: "1986", bwa: "1987", bvt: "1988", bra: "1989", iot: "1990", vgb: "1991", brn: "1992", bgr: "1993", bfa: "1994", bdi: "1995", khm: "1996", cmr: "1997", can: "1998", cpv: "1999", cym: "2000", caf: "2001", tcd: "2002", chl: "2003", chn: "2004", cxr: "2005", cck: "2006", col: "2007", com: "2008", cog: "2009", cod: "2010", cok: "2011", cri: "2012", civ: "2067", hrv: "2013", cub: "2014", cuw: "2015", cyp: "2016", cze: "2017", dnk: "2018", dji: "2019", dma: "2020", dom: "2021", ecu: "2022", egy: "2023", slv: "2024", gnq: "2025", eri: "2026", est: "2027", swz: "2172", eth: "2028", flk: "2029", fro: "2030", fji: "2031", fin: "2032", fra: "2033", guf: "2034", pyf: "2035", atf: "2036", gab: "2037", gmb: "2038", geo: "2039", deu: "2040", gha: "2041", gib: "2042", grc: "2043", grl: "2044", grd: "2045", glp: "2046", gum: "2047", gtm: "2048", gin: "2050", gnb: "2051", guy: "2052", hti: "2053", hmd: "2054", hnd: "2055", hkg: "2056", hun: "2057", isl: "2058", ind: "2059", idn: "2060", irn: "2061", irq: "2062", irl: "2063", imn: "2064", isr: "2065", ita: "2066", jam: "2068", jpn: "2069", jor: "2071", kaz: "2072", ken: "2073", kir: "2074", kwt: "2075", kgz: "2076", lao: "2077", lva: "2078", lbn: "2079", lso: "2080", lbr: "2081", lby: "2082", lie: "2083", ltu: "2084", lux: "2085", mac: "2086", mkd: "2087", mdg: "2088", mwi: "2089", mys: "2090", mdv: "2091", mli: "2092", mlt: "2093", mhl: "2094", mtq: "2095", mrt: "2096", mus: "2097", myt: "2098", mex: "2099", fsm: "2100", mda: "2101", mco: "2102", mng: "2103", mne: "2104", msr: "2105", mar: "2106", moz: "2107", mmr: "2108", nam: "2109", nru: "2110", npl: "2111", nld: "2112", ant: "2113", ncl: "2114", nzl: "2115", nic: "2116", ner: "2117", nga: "2118", niu: "2119", nfk: "2120", prk: "2121", mnp: "2122", nor: "2123", omn: "2124", pak: "2125", plw: "2126", pse: "2127", pan: "2128", png: "2129", pry: "2130", per: "2131", phl: "2132", pcn: "2133", pol: "2134", prt: "2135", pri: "2136", qat: "2137", reu: "2138", rou: "2139", rus: "2140", rwa: "2141", blm: "2142", shn: "2143", kna: "2144", lca: "2145", maf: "2146", spm: "2147", vct: "2148", wsm: "2149", smr: "2150", stp: "2151", sau: "2152", sen: "2153", srb: "2154", syc: "2155", sle: "2156", sgp: "2157", svk: "2159", svn: "2160", slb: "2161", som: "2162", zaf: "2163", sgs: "2164", kor: "2165", ssd: "2166", esp: "2167", lka: "2168", sdn: "2169", sur: "2170", sjm: "2171", swe: "2173", che: "2174", syr: "2175", twn: "2176", tjk: "2177", tza: "2178", tha: "2179", tls: "2180", tgo: "2181", tkl: "2182", ton: "2183", tto: "2184", tun: "2185", tur: "2186", tkm: "2187", tca: "2188", tuv: "2189", vir: "2190", uga: "2191", ukr: "2192", are: "2193", gbr: "2194", umi: "2195", ury: "2196", usa: "2197", uzb: "2198", vut: "2199", vat: "2200", ven: "2201", vnm: "2202", wlf: "2203", esh: "2204", yem: "2205", zmb: "2206", zwe: "2207"};
+const caCountryIds = {afago: "1964", afbdi: "1995", afben: "1981", afbfa: "1994", afbwa: "1987", afcaf: "2001", afciv: "2067", afcmr: "1997", afcod: "2010", afcog: "2009", afcom: "2008", afcpv: "1999", afdji: "2019", afdza: "1961", afegy: "2023", aferi: "2026", afesh: "2204", afeth: "2028", afgab: "2037", afgha: "2041", afgin: "2050", afgmb: "2038", afgnb: "2051", afgnq: "2025", afken: "2073", aflbr: "2081", aflby: "2082", aflso: "2080", afmar: "2106", afmdg: "2088", afmli: "2092", afmoz: "2107", afmrt: "2096", afmus: "2097", afmwi: "2089", afmyt: "2098", afnam: "2109", afner: "2117", afnga: "2118", afreu: "2138", afrwa: "2141", afsdn: "2169", afsen: "2153", afshn: "2143", afsle: "2156", afsom: "2162", afssd: "2166", afstp: "2151", afswz: "2172", afsyc: "2155", aftcd: "2002", aftgo: "2181", aftun: "2185", aftza: "2178", afuga: "2191", afzaf: "2163", afzmb: "2206", afzwe: "2207", anata: "1966", anatf: "2036", anbvt: "1988", anhmd: "2054", ansgs: "2164", asafg: "1958", asare: "2193", asarm: "1969", asaze: "1973", asbgd: "1976", asbhr: "1975", asbrn: "1992", asbtn: "1983", ascck: "2006", aschn: "2004", ascxr: "2005", ascyp: "2016", asgeo: "2039", ashkg: "2056", asidn: "2060", asind: "2059", asiot: "1990", asirn: "2061", asirq: "2062", asisr: "2065", asjor: "2071", asjpn: "2069", askaz: "2072", askgz: "2076", askhm: "1996", askor: "2165", askwt: "2075", aslao: "2077", aslbn: "2079", aslka: "2168", asmac: "2086", asmdv: "2091", asmmr: "2108", asmng: "2103", asmys: "2090", asnpl: "2111", asomn: "2124", aspak: "2125", asphl: "2132", asprk: "2121", aspse: "2127", asqat: "2137", assau: "2152", assgp: "2157", assyr: "2175", astha: "2179", astjk: "2177", astkm: "2187", astls: "2180", astur: "2186", astwn: "2176", asuzb: "2198", asvnm: "2202", asyem: "2205", eualb: "1960", euand: "1963", euaut: "1972", eubel: "1979", eubgr: "1993", eubih: "1986", eublr: "1978", euche: "2174", eucze: "2017", eudeu: "2040", eudnk: "2018", euesp: "2167", euest: "2027", eufin: "2032", eufra: "2033", eufro: "2030", eugbr: "2194", eugib: "2042", eugrc: "2043", euhrv: "2013", euhun: "2057", euimn: "2064", euirl: "2063", euisl: "2058", euita: "2066", eulie: "2083", eultu: "2084", eulux: "2085", eulva: "2078", eumco: "2102", eumda: "2101", eumkd: "2087", eumlt: "2093", eumne: "2104", eunld: "2112", eunor: "2123", eupol: "2134", euprt: "2135", eurou: "2139", eurus: "2140", eusjm: "2171", eusmr: "2150", eusrb: "2154", eusvk: "2159", eusvn: "2160", euswe: "2173", euukr: "2192", euvat: "2200", naabw: "1970", naaia: "1965", naant: "2113", naatg: "1967", nabes: "1985", nabhs: "1974", nablm: "2142", nablz: "1980", nabmu: "1982", nabrb: "1977", nacan: "1998", nacri: "2012", nacub: "2014", nacuw: "2015", nacym: "2000", nadma: "2020", nadom: "2021", nagrd: "2045", nagrl: "2044", nagtm: "2048", nahnd: "2055", nahti: "2053", najam: "2068", nakna: "2144", nalca: "2145", namaf: "2146", namex: "2099", namsr: "2105", namtq: "2095", nanic: "2116", napan: "2128", napri: "2136", naslv: "2024", naspm: "2147", natca: "2188", natto: "2184", naumi: "2195", nausa: "2197", navct: "2148", navgb: "1991", navir: "2190", ocasm: "1962", ocaus: "1971", occok: "2011", ocfji: "2031", ocfsm: "2100", ocglp: "2046", ocgum: "2047", ockir: "2074", ocmhl: "2094", ocmnp: "2122", ocncl: "2114", ocnfk: "2120", ocniu: "2119", ocnru: "2110", ocnzl: "2115", ocpcn: "2133", ocplw: "2126", ocpng: "2129", ocpyf: "2035", ocslb: "2161", octkl: "2182", octon: "2183", octuv: "2189", ocvut: "2199", ocwlf: "2203", ocwsm: "2149", saarg: "1968", sabol: "1984", sabra: "1989", sachl: "2003", sacol: "2007", saecu: "2022", saflk: "2029", saguf: "2034", saguy: "2052", saper: "2131", sapry: "2130", sasur: "2170", saury: "2196", saven: "2201"};
 
 module.exports = function(app) {
   app.get("/api/companies/ca", async(req, res) => {
-    const caCountryFilterId = caCountryIds[req.query.origin] || "1968";
-    const x = await axios.get("http://qat02-ws.connectamericas.com/apirest/v6/company_by_hscode", {
-      params: {
-        hs_code: req.query.hs_code || "0805",
-        origin: caCountryFilterId,
-        location: "origin"
-      },
-      headers: {
-        clientToken: "oec-FamulJKGa7"
+    const caCountryFilterId = req.query.country ? caCountryIds[req.query.country.toLowerCase()] || "1968" : null;
+    const caHsFilterId = req.query.hs_code && !isNaN(req.query.hs_code) ? req.query.hs_code.length % 2 ? req.query.hs_code.slice(1) : req.query.hs_code.slice(2) : null;
+    if (caHsFilterId) {
+      const companyByHsResp = await axios.get("http://qat02-ws.connectamericas.com/apirest/v6/company_by_hscode", {
+        params: caCountryFilterId && caHsFilterId
+          ? {
+            page_size: 100,
+            hs_code: caHsFilterId || "0805",
+            origin: caCountryFilterId,
+            location: "origin"
+          }
+          : {
+            page_size: 100,
+            hs_code: caHsFilterId || "0805"
+          },
+        headers: {
+          clientToken: "oec-FamulJKGa7"
+        }
+      }).catch(err => (console.log(err), {companies: []}));
+      const companiesWithLogo = companyByHsResp.data ? companyByHsResp.data.companies.filter(d => d.logo) : [];
+      return res.json({total_items: companiesWithLogo.length, companies: companiesWithLogo});
+    }
+    let allCompanies = [];
+    let totalItems = 0;
+    for (let i = 0; i < 5; i++) {
+      const offset = i * 60;
+      const companyByCountryResp = await axios.get("http://qat02-ws.connectamericas.com/apirest/v6/company", {
+        params: {
+          items_per_page: 60,
+          offset,
+          country: caCountryFilterId,
+          verified: 1
+        },
+        headers: {
+          clientToken: "oec-FamulJKGa7"
+        }
+      }).catch(err => (console.log(err), {_embedded: {company: []}}));
+      if (companyByCountryResp.data && companyByCountryResp.data._embedded) {
+        if (companyByCountryResp.data.total_items === 0) {
+          break;
+        }
+        const thisIterationOfCompanies = companyByCountryResp.data._embedded.company
+          .filter(d => d.photo_url)
+          .map(d => ({id: d.id, name: d.name, industry: d.industry, photo_url: d.photo_url, business_objectives: d.business_objectives, public_url: d.public_url}));
+        allCompanies = allCompanies.concat(thisIterationOfCompanies);
+        totalItems += thisIterationOfCompanies.length;
       }
-    });
-    return res.json(x.data);
+    }
+
+    return res.json({total_items: totalItems, companies: allCompanies});
   });
 };
