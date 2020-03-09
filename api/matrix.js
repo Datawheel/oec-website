9f6bf38046a94eb165cad05320ac73e5403c5186
Dave Landry <landry.dave@gmail.com>
2020-03-09 12:55:10 -0400

    adds JWT to data matrix API route

--- api/matrix.js
+++ api/matrix.js
@@ -1,8 +1,10 @@
 const axios = require("axios");
 const yn = require("yn");
+const jwt = require("jsonwebtoken");
 const {timeFormat} = require("d3-time-format");
 
 let {CANON_CMS_CUBES} = process.env;
+const {OLAP_PROXY_SECRET} = process.env;
 if (CANON_CMS_CUBES.substr(-1) === "/") CANON_CMS_CUBES = CANON_CMS_CUBES.substr(0, CANON_CMS_CUBES.length - 1);
 const verbose = yn(process.env.CANON_CMS_LOGGING);
 
@@ -11,23 +13,23 @@ const FEATURE_MATRIX = {
     products: [
       {
         cubeName: "trade_i_baci_a_02",
-        fullName: "International Trade Data (BACI 02)"
+        fullName: "International Trade (BACI 02)"
       },
       {
         cubeName: "trade_i_baci_a_12",
-        fullName: "International Trade Data (BACI 12)"
+        fullName: "International Trade (BACI 12)"
       },
       {
         cubeName: "trade_i_baci_a_07",
-        fullName: "International Trade Data (BACI 07)"
+        fullName: "International Trade (BACI 07)"
       },
       {
         cubeName: "trade_i_baci_a_96",
-        fullName: "International Trade Data (BACI 96)"
+        fullName: "International Trade (BACI 96)"
       },
       {
         cubeName: "trade_i_baci_a_92",
-        fullName: "International Trade Data (BACI 92)"
+        fullName: "International Trade (BACI 92)"
       },
       {
         cubeName: "trade_i_comtrade_a_sitc2",
@@ -61,55 +63,55 @@ const FEATURE_MATRIX = {
     products: [
       {
         cubeName: "trade_s_usa_district_m_hs",
-        fullName: "USA Trade Data"
+        fullName: "United States Trade"
       },
       {
         cubeName: "trade_s_chn_m_hs",
-        fullName: "China Trade Data"
+        fullName: "China Trade"
       },
       {
         cubeName: "trade_s_bra_mun_m_hs",
-        fullName: "Brazil Trade Data"
+        fullName: "Brazil Trade"
       },
       {
         cubeName: "trade_s_can_m_hs",
-        fullName: "Canada Trade Data"
+        fullName: "Canada Trade"
       },
       {
         cubeName: "trade_s_esp_m_hs",
-        fullName: "Spain Trade Data"
+        fullName: "Spain Trade"
       },
       {
         cubeName: "trade_s_deu_m_egw",
-        fullName: "Germany Trade Data"
+        fullName: "Germany Trade"
       },
       {
         cubeName: "trade_s_tur_m_hs",
-        fullName: "Turkey Trade Data"
+        fullName: "Turkey Trade"
       },
       {
         cubeName: "trade_s_swe_m_hs",
-        fullName: "Sweden Trade Data"
+        fullName: "Sweden Trade"
       },
       {
         cubeName: "trade_s_ury_a_hs",
-        fullName: "Uruguay Trade Data"
+        fullName: "Uruguay Trade"
       },
       {
         cubeName: "trade_s_jpn_m_hs",
-        fullName: "Japan Trade Data"
+        fullName: "Japan Trade"
       },
       {
         cubeName: "trade_s_chl_m_hs",
-        fullName: "Chile Trade Data"
-      },  
+        fullName: "Chile Trade"
+      },
       {
         cubeName: "trade_s_rus_m_hs",
-        fullName: "Brazil Trade Data"
+        fullName: "Brazil Trade"
       },
       {
         cubeName: "trade_s_zaf_m_hs",
-        fullName: "South Africa Trade Data"
+        fullName: "South Africa Trade"
       }
     ]
   }
@@ -120,17 +122,17 @@ const FEATURE_MATRIX = {
 {
   cubeName: "patents_s_usa_w_cpc",
   fullName: "t"
-  grouping: 
+  grouping:
 },
 {
   cubeName: "patents_i_uspto_w_cpc",
   fullName: "t"
-  grouping: 
+  grouping:
 },
 {
   cubeName: "patents_i_uspto_w_organizations",
   fullName: "t"
-  grouping: 
+  grouping:
 },
 */
 
@@ -165,16 +167,28 @@ const dateFormat = d => {
 
 module.exports = function(app) {
 
-  const {db} = app.settings;
-
   app.get("/api/matrix", async(req, res) => {
 
     const url = `${CANON_CMS_CUBES}/cubes`;
 
-    const cubeData = await axios.get(url).then(d => d.data).catch(catcher);
+    const apiToken = jwt.sign(
+      {
+        auth_level: 10,
+        sub: "server",
+        status: "valid"
+      },
+      OLAP_PROXY_SECRET,
+      {expiresIn: "30m"}
+    );
 
-    const matrix = [];
-    
+    const config = {
+      headers: {
+        "x-tesseract-jwt-token": apiToken
+      }
+    };
+
+    const cubeData = await axios.get(url, config).then(d => d.data).catch(catcher);
+
     if (cubeData && cubeData.cubes) {
       for (const nation of Object.keys(FEATURE_MATRIX)) {
         const thisNation = FEATURE_MATRIX[nation];
@@ -183,10 +197,10 @@ module.exports = function(app) {
           const newGroup = [];
           for (const meta of thisGroup) {
             const thisCube = cubeData.cubes.find(cube => cube.name === meta.cubeName);
-            const {name, dimensions, measures, annotations} = thisCube;
+            const {name, dimensions, measures} = thisCube;
             let drilldown, measure, timeResolution;
             if (measures[0]) measure = measures[0].name;
-            
+
             // Determine the Time Resolution
             const names = dimensions.map(d => d.name);
             if (names.includes("Year")) {
@@ -201,10 +215,10 @@ module.exports = function(app) {
                   timeResolution = "Year";
                 }
                 else {
-                  timeResolution = levels[levels.length - 2].name;  
+                  timeResolution = levels[levels.length - 2].name;
                 }
               }
-              catch (e) { 
+              catch (e) {
                 catcher(e);
               }
             }
@@ -222,15 +236,17 @@ module.exports = function(app) {
               const isProduct = d.name.includes("Product") || d.hierarchies[0].name.includes("Product");
               const isService = d.name.includes("Service") || d.hierarchies[0].name.includes("Service");
               const topLevel = isProduct || isService ? "product" : "geography";
-              const resolution = d.hierarchies[0].levels.reduce((acc2, d2) => 
-                acc2.concat(d2.annotations && d2.annotations.level ? d2.annotations.level : d2.name), []
-              ).join(", ");
-              return {...acc, [topLevel]: resolution};  
+              const resolution = d.hierarchies[0].levels.reduce((acc2, d2) => {
+                let title = d2.annotations && d2.annotations.level ? d2.annotations.level : d2.name;
+                title = title.replace(" Product", "");
+                return acc2.concat(title);
+              }, []).join(", ");
+              return {...acc, [topLevel]: resolution};
             }, {time: `${timeResolution}ly`});
 
             if (!name || !drilldown || !measure) continue;
             const rangeURL = `${CANON_CMS_CUBES}/data.jsonrecords?cube=${name}&drilldowns=${drilldown}&measures=${measure}&parents=false&sparse=false`;
-            const range = await axios.get(rangeURL).then(d => d.data.data).catch(catcher);
+            const range = await axios.get(rangeURL, config).then(d => d.data.data).catch(catcher);
             if (range && Array.isArray(range)) {
               const start = dateFormat(range[0][drilldown]);
               const end = dateFormat(range[range.length - 1][drilldown]);
