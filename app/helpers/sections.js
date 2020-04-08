eab1e020bd21e9ae326410cf4e6297b28fa54199
ffigueroal <felipe.figueroa.lara@gmail.com>
2020-04-08 18:42:51 -0400

    fix color logic on tooltip and legend tooltips

---
+++ app/helpers/sections.js
@@ -0,0 +1,38 @@
+module.exports = {
+  hsSections: [
+    "Animal Products",
+    "Vegetable Products",
+    "Animal and Vegetable Bi-Products",
+    "Foodstuffs",
+    "Mineral Products",
+    "Chemical Products",
+    "Plastics and Rubbers",
+    "Animal Hides",
+    "Wood Products",
+    "Paper Goods",
+    "Textiles",
+    "Footwear and Headwear",
+    "Stone And Glass",
+    "Precious Metals",
+    "Metals",
+    "Machines",
+    "Transportation",
+    "Instruments",
+    "Weapons",
+    "Miscellaneous",
+    "Arts and Antiques"
+  ],
+  sitcSections: [
+    "Food and live animals",
+    "Beverages and tobacco",
+    "Crude materials, inedible, except fuels",
+    "Mineral fuels, lubricants and related materials",
+    "Animal and vegetable oils, fats and waxes",
+    "Chemicals and related products, n.e.s.",
+    "Manufactured goods classified chiefly by material",
+    "Machinery and transport equipment",
+    "Miscellaneous manufactured articles",
+    "Commodities and transactions not classified elsewhere in the SITC",
+    "Undefined"
+  ]
+};
