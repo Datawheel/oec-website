4c2f6f6fcc97676fbf49f379f334283037be8fa9
cnavarreteliz <cnavarreteliz@gmail.com>
2020-04-16 17:25:12 -0400

    [vizbuilder] fixes bug in vbTitle function

--- app/helpers/vbTitle.js
+++ app/helpers/vbTitle.js
@@ -55,11 +55,9 @@ export const getVbTitle = (items, axis, routeParams) => {
     };
 
     title = networkTitleOptions[flow][0] || networkTitleOptions.export[0];
-    // params = networkTitleOptions[flow][1] || networkTitleOptions.export[1];
   }
   else if (chart === "rings") {
     title = "vb_title_rings";
-    // params = {country: _countryNames, product: _productNames, time};
   }
   else if (chart === "scatter") {
     title = "vb_title_scatter";
@@ -69,37 +67,29 @@ export const getVbTitle = (items, axis, routeParams) => {
     title = isPartner
       ? "vb_title_trade_balance_partner"
       : "vb_title_trade_balance";
-
-    // params = isPartner ? {country: _countryNames, partner: _partnerNames, time} : {country: _countryNames, time};
   }
   else if (isTrade) {
     // Titles for Trade charts
     if (!isCountry && isProduct) {
       title = "vb_title_which_countries_flow_product";
-      // params = {flow, product: _productNames, time};
     }
     else if (isCountry && isProduct) {
       title = "vb_title_where_country_flow_product";
-      // params = {country: _countryNames, flow, time, product: _productNames, prep: preps[flow]};
     }
     else if (isGeoGrouping) {
       title = "vb_title_where_country_flow";
-      // params = {country: _countryNames, flow, time, prep: preps[flow]};
     }
     else if (isCountry && isPartner) {
       title = "vb_title_what_country_flow_partner";
-      // params = {country: _countryNames, partner: _partnerNames, flow, time};
     }
   }
   else {
     // Titles for Technology charts
     if (isCountry) {
       title = "vb_title_what_country_patent";
-      // params = {country: _countryNames, time};
     }
     else if (!isCountry && isProduct) {
       title = "vb_title_which_countries_patent";
-      // params = {names: _technologyNames, time};
     }
   }
 
