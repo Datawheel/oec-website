de9f21e59e1f4e8a195a1ed0f448f44aca1d46cf
Alan Ramirez <alanramirezb@gmail.com>
2020-04-28 12:36:50 -0400

    Adds spain geo map

--- app/App.jsx
+++ app/App.jsx
@@ -2,10 +2,12 @@ import React, {Component, Fragment} from "react";
 import {connect} from "react-redux";
 import {isAuthenticated} from "@datawheel/canon-core";
 import "./App.css";
+import {geoConicConformalSpain} from "d3-composite-projections";
 
 class App extends Component {
   componentWillMount() {
     this.props.isAuthenticated();
+    if (typeof window !== "undefined") window.geoConicConformalSpain = geoConicConformalSpain;
   }
   render() {
     return <Fragment>
