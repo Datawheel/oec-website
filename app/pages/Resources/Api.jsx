1e387832812e1855eed96a7d9b4560a26e1d42fd
Alexander Simoes <alexandersimoes@gmail.com>
2020-04-28 14:02:23 -0400

    separates data links into top level nav item

--- app/pages/Resources/Api.jsx
+++ app/pages/Resources/Api.jsx
@@ -10,6 +10,30 @@ export default class Api extends Component {
 
         <h1> API </h1>
 
+        <h2>Coming soon...</h2>
+        <p>
+          Documentation for the public API is still in development. In the meantime, the documentation for the legacy (OEC v3.0) documenation is still available here: <a href="https://oec.world/api/" target="_blank" rel="noopener noreferrer">OEC 3.0 API Documentation</a>.
+        </p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </div>
     );
   }
