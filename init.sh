bdcc92bb961fca7e63bde5071fc95d6a0e0dd911
cnavarreteliz <cnavarreteliz@gmail.com>
2020-04-29 11:21:21 -0400

    creates script for cleaning cache of canon-stats

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@ npm ci
 CANON_CONST_BASE='https://api.oec.world/tesseract/data' node --max_old_space_size=6000 `which npm` run build
 pm2 restart ecosystem.config.js --update-env
 pm2 save
+python3 clear_cache.py
