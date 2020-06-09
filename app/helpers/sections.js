936ea5363464def089fb532d49050fe3e0f9ea8d
ffigueroal <felipe.figueroa.lara@gmail.com>
2020-06-09 19:04:58 -0400

    fixes bug color in bolivia subnational profile

--- app/helpers/sections.js
+++ app/helpers/sections.js
@@ -24,16 +24,16 @@ module.exports = {
     "Unspecified"
   ],
   sitcSections: [
-    "Food and live animals",
-    "Beverages and tobacco",
-    "Crude materials, inedible, except fuels",
-    "Mineral fuels, lubricants and related materials",
-    "Animal and vegetable oils, fats and waxes",
-    "Chemicals and related products, n.e.s.",
-    "Manufactured goods classified chiefly by material",
-    "Machinery and transport equipment",
-    "Miscellaneous manufactured articles",
-    "Commodities and transactions not classified elsewhere in the SITC",
+    " Food and live animals",
+    " Beverages and tobacco",
+    " Crude materials, inedible, except fuels",
+    " Mineral fuels, lubricants and related materials",
+    " Animal and vegetable oils, fats and waxes",
+    " Chemicals and related products, n.e.s.",
+    " Manufactured goods classified chiefly by material",
+    " Machinery and transport equipment",
+    " Miscellaneous manufactured articles",
+    " Commodities and transactions not classified elsewhere in the SITC",
     "Undefined"
   ]
 };
